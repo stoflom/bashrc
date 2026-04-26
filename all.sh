878630f65926a68a42f3c745d42d1528ee28e471
Christoffel Lombard <stoflom@gmail.com>
2026-04-26 10:13:21 +0200

    Fix some sillyness

--- all.sh
+++ all.sh
@@ -12,7 +12,7 @@ logout() {
 	DISPLAY=:0 gnome-session-quit --force
 }
 
-# Fix less for utf8 and colorfull
+# Fix less for utf8 and color
 export LESSCHARSET=utf-8
 alias less='less -R'
 
@@ -43,3 +43,6 @@ fi
 # shortcuts for disks to use: e.g. cd "$T7"      (note quotes "")
 export MECER="/run/media/stoflom/MecerExternal"
 export T7="/run/media/stoflom/T7Shield"
+
+#Fix some sillyness
+alias reboot='sudo systemctl reboot'
